53db546d7c5ea241b72fd56765746e280135391d
Aarti Jangid <jangid.aarti08@gmail.com>
2020-03-31 17:20:52 +0200

    added for indexing data in repository

--- src/main/kotlin/rt/string/PalindromeString.kt
+++ src/main/kotlin/rt/string/PalindromeString.kt
@@ -1,8 +1,11 @@
 package rt.string
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
 
 @RedisHash("PalindromeString")
-data class PalindromeString (val input: String, val isPalindrome: Boolean){
-
+data class PalindromeString (@Indexed val input: String, val isPalindrome: Boolean){
+    @get:Id
+    var id: String? = null
 }
